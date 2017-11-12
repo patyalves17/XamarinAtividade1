ed1c7942aa76af47ca0495c7aa6e5d3ec4f7880f
ABC Education <abceducation@AirAbc.local>
2017-11-12 11:55:08 -0200

    xamarin atividade 1

---
+++ Main/ConfigPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+using Xamarin.Forms;
+
+namespace Main
+{
+    public partial class ConfigPage : ContentPage
+    {
+        bool recebe;
+        String email;
+
+
+        public String Email{
+            get { return email; }
+            set { 
+                email = value;
+            }
+        }
+
+        public bool Recebe {
+            get { return recebe;  }
+            set { recebe = value; }
+        }
+
+        public ConfigPage()
+        {
+            InitializeComponent();
+            swRecebe_Switched(null, null);
+        }
+
+        public void swRecebe_Switched(Object sender, EventArgs e){
+            if(swfCell.On){
+                entryEmail.IsEnabled = true;
+                recebe = true;
+            }else{
+                entryEmail.IsEnabled = false;
+                recebe = false;
+            }
+        }
+
+        public void email_Completed(Object sender, EventArgs e){
+            email = entryEmail.Text;
+        }
+
+     
+
+       
+
+    }
+}
---
+++ Main/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Main
+{
+    public partial class MainPage : ContentPage
+    {
+        ConfigPage cp;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            cp = new ConfigPage();
+        }
+
+        private void btnEnviar_Clicked (object sender, EventArgs e) {
+            //Navigation.PushAsync(new EnviarPage());
+            if(cp.Recebe){
+                DisplayAlert("Enviar email",string.Format("Email enviado para {0}", cp.Email), "OK");
+            }else{
+                DisplayAlert("Enviar email", "Não foi possivel enviar o email", "OK");
+            }
+        }
+
+        private void btnConfig_Clicked(object sender, EventArgs e) {
+            Navigation.PushAsync(cp);
+        }
+    }
+}
